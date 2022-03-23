78a2b7e5be964e58b4614b2cf15409932203ee4f
NUNAVATHKUMAR <npk1476@gmail.com>
2022-03-23 15:52:28 +0530

    Done

---
+++ 3_Implementation/source/addition.c
@@ -0,0 +1,8 @@
+
+#include<stdio.h>
+#include<math.h>
+#include "funcation.h"
+int addition(int a, int b)
+{
+ return (a+b); //returning  result to main funtion 
+}
---
+++ 3_Implementation/source/division.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int division(int a, int b)
+{
+    
+    return (a/b);
+}
